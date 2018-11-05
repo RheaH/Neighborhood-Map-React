4aaf48a411713c4a1b0f8469da3d899a0ea68aec
RheaH <39145040+RheaH@users.noreply.github.com>
2018-11-05 15:26:40 -0500

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const neighborhood = {
+
+        lat: 35.686975,
+        lng: -105.937799
+
+}
+
+export const locations =
+    [
+        {title: 'The Plaza', location: {lat: 35.686929, lng: -105.938104}},
+        {title: 'La Cieneguilla Petroglyph Site', location: {lat: 35.635784,  lng: -106.044019 }},
+        {title: 'El Rancho de las Golondrinas', location: {lat: 35.570384,  lng: -106.110771 }},
+        {title: 'Canyon Road', location: {lat: 35.683821,  lng: -105.933614 }},
+        {title: 'Pilar Yacht Club', location: {lat: 36.269198,  lng: -105.787329 }}
+      ]
---
+++ src/mapConfig.js
@@ -0,0 +1,66 @@
+const mapConfig = {
+    zoom: 15,
+    mapTypeControl: false,
+    styles: [
+      {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },{
+                featureType: 'landscape.natural',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+              },{
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },{
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -40 }
+            ]
+          },{
+            featureType: 'transit.station',
+            stylers: [
+              { weight: 9 },
+              { hue: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+              { visibility: 'off' }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { lightness: 100 }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { lightness: -100 }
+            ]
+          },{
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+              { visibility: 'on' },
+              { color: '#f0e4d3' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -25 }
+            ]
+          }
+        ]
+  };
+
+export default mapConfig;
